0ba2ec03d06545a4a1216dc863cc9df8f1e83251
FriggyStudios <friggystudios@gmail.com>
2021-03-07 15:29:08 +0000

    build .lib for testing. include built .lib and cudart.lib for testing. run tests manually in main.cpp, as the tests were not running after linking the built .lib

---
+++ SimulationTest/main.cpp
@@ -0,0 +1,13 @@
+﻿#include "pch.h"
+
+int main(int argc) {
+	char** argv2 = new char* [1];
+
+	for (unsigned int i = 0; i < 1; i++) {
+		// str is likely to be an array of characters
+		argv2[i] = "str";
+	}
+
+	::testing::InitGoogleTest(&argc, argv2);
+	return RUN_ALL_TESTS();
+}
