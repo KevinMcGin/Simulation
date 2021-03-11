3c0efcb02c0afe891e7a2f301f69b1718cdc72c6
FriggyStudios <friggystudios@gmail.com>
2021-03-11 21:18:51 +0000

    allow option no cuda on build

--- Simulation-CMake/build/cmake_install.cmake
+++ Simulation-CMake/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/test/source/repos/Simulation
+# Install script for directory: F:/workspace/Simulation/Simulation-CMake
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/test/source/repos/Simulation/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "F:/workspace/Simulation/Simulation-CMake/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
