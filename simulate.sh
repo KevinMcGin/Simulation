f6dd71c37039144e38f6481b13dcc80c9074798f
FriggyStudios <friggystudios@gmail.com>
2021-03-13 23:19:13 +0000

    batch to bash

---
+++ simulate.sh
@@ -0,0 +1,3 @@
+./compile.sh
+./build/bin/Debug/SimulationMain.exe
+./render.sh
---
+++ test.sh
@@ -0,0 +1,4 @@
+./compile.sh
+cd build
+ctest -C Debug --output-on-failure
+# REM ./build\bin\Debug\SimulationTest.exe
