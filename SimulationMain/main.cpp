47fb854999361df7dd2cfcac5420b8058235a2b1
FriggyStudios <friggystudios@gmail.com>
2021-03-13 12:27:42 +0000

    central star

--- SimulationMain/main.cpp
+++ SimulationMain/main.cpp
@@ -11,31 +11,34 @@
 
 int main()
 {
-	double particleCount = 12000;
-	unsigned int particleQuadrantCount = particleCount /5.0;
-	double meanMass = 0.03;
+	double particleCount = 1000;
+	unsigned int particleQuadrantCount = particleCount / 4.0;
+	double meanMass = 0.01;
 	Distribution* massDistrubtion = new DistributionSimple(meanMass, meanMass*0.9);
-	Distribution* density = new DistributionValue(10000);
+	Distribution* density = new DistributionValue(30000);
 	DistributionDensity* densityDistribution = new DistributionMassDensity(massDistrubtion, density);
-	double maxSpeed = 0.03;
-	double deltaSpeed = maxSpeed * 0.2;
-	Distribution3D* positionDistrubtion1 = new DistributionCircle({ -1,0,0 }, 2);
-	Distribution3D* positionDistrubtion2 = new DistributionCircle({ 1,0,0 }, 2);
-	Distribution3D* positionDistrubtion3 = new DistributionCircle({ 0,1,0 }, 2);
-	Distribution3D* positionDistrubtion4 = new DistributionCircle({ 0,-1,0 }, 2);
-	Distribution3D* positionDistrubtion5 = new DistributionCircle({ 0,0,0 }, 2);
-	Distribution3D* velocityDistrubtion1 = new DistributionCircle({ 0,-maxSpeed,0 }, deltaSpeed);
-	Distribution3D* velocityDistrubtion2 = new DistributionCircle({ 0,maxSpeed,0 }, deltaSpeed);
-	Distribution3D* velocityDistrubtion3 = new DistributionCircle({ -maxSpeed,0,0 }, deltaSpeed);
-	Distribution3D* velocityDistrubtion4 = new DistributionCircle({ maxSpeed,0,0 }, deltaSpeed);
+	DistributionDensity* distributionDensityStar = new DistributionMassDensity(new DistributionValue(50), density);
+	double meanSpeed = 0.03;
+	double deltaSpeed = meanSpeed * 0.2;
+	double meanDistance = 2.5;
+	double deltaDistance = 2.4;
+	Distribution3D* positionDistrubtion1 = new DistributionCircle({ -meanDistance,0,0 }, deltaDistance);
+	Distribution3D* positionDistrubtion2 = new DistributionCircle({ meanDistance,0,0 }, deltaDistance);
+	Distribution3D* positionDistrubtion3 = new DistributionCircle({ 0,meanDistance,0 }, deltaDistance);
+	Distribution3D* positionDistrubtion4 = new DistributionCircle({ 0,-meanDistance,0 }, deltaDistance);
+	Distribution3D* positionDistrubtion5 = new DistributionCircle({ 0,0,0 }, 0);
+	Distribution3D* velocityDistrubtion1 = new DistributionCircle({ 0,-meanSpeed,0 }, deltaSpeed);
+	Distribution3D* velocityDistrubtion2 = new DistributionCircle({ 0,meanSpeed,0 }, deltaSpeed);
+	Distribution3D* velocityDistrubtion3 = new DistributionCircle({ -meanSpeed,0,0 }, deltaSpeed);
+	Distribution3D* velocityDistrubtion4 = new DistributionCircle({ meanSpeed,0,0 }, deltaSpeed);
 	Distribution3D* velocityDistrubtion5 = new DistributionCircle({ 0,0,0 }, 0);
 	Distribution3D* angularVelocityDistrubtion = new DistributionCircle({ 0,0,0 }, 0);
 	ParticleDistributionSimple particleDistribution1(densityDistribution, positionDistrubtion1, velocityDistrubtion1, angularVelocityDistrubtion);
 	ParticleDistributionSimple particleDistribution2(densityDistribution, positionDistrubtion2, velocityDistrubtion2, angularVelocityDistrubtion);
 	ParticleDistributionSimple particleDistribution3(densityDistribution, positionDistrubtion3, velocityDistrubtion3, angularVelocityDistrubtion);
 	ParticleDistributionSimple particleDistribution4(densityDistribution, positionDistrubtion4, velocityDistrubtion4, angularVelocityDistrubtion);
-	ParticleDistributionSimple particleDistribution5(densityDistribution, positionDistrubtion5, velocityDistrubtion5, angularVelocityDistrubtion);
-	SimulationInput* input = new SimulationInputRandomSimple({ particleQuadrantCount, particleQuadrantCount, particleQuadrantCount, particleQuadrantCount, particleQuadrantCount },
+	ParticleDistributionSimple particleDistribution5(distributionDensityStar, positionDistrubtion5, velocityDistrubtion5, angularVelocityDistrubtion);
+	SimulationInput* input = new SimulationInputRandomSimple({ particleQuadrantCount, particleQuadrantCount, particleQuadrantCount, particleQuadrantCount, 1 },
 		{ particleDistribution1, particleDistribution2, particleDistribution3, particleDistribution4, particleDistribution5 });
 
 	/*SimulationInput* input = new SimulationInputRandomSimple({ 1, 1 },
