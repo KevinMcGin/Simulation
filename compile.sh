d5bd784ca7c514da22dfcd3b3207adfb44ab2c42
FriggyStudios <friggystudios@gmail.com>
2021-03-14 20:31:39 +0000

    print compile failed

--- compile.sh
+++ compile.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env sh
 cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Debug -DUSE_CUDA=ON
+if [ $? -eq 1 ]
+then
+   echo -e "\ncompile failed"
+   exit
+fi
 cmake --build build
+if [ $? -eq 1 ]
+then
+   echo -e "\ncompile failed"
+   exit
+fi
