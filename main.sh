eb27f5c7a6a3ef48390cae1aef3896c26e5572b4
FriggyStudios <friggystudios@gmail.com>
2021-03-14 12:38:04 +0000

    stop scripts on non zero response

--- main.sh
+++ main.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env sh
 ./compile.sh
+if [ $? -eq 1 ]
+then
+   echo -e "\ncompile failed"
+   exit
+fi
 ./build/bin/Debug/SimulationMain.exe
+if [ $? -eq 1 ]
+then
+   echo -e "\nmain failed"
+   exit
+fi
 ./render.sh
+
+echo "end"
+echo $?
--- test.sh
+++ test.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 ./compile.sh
+if [ $? -eq 1 ]
+then
+   echo -e "\ncompile failed"
+   exit
+fi
 cd build
 ctest -C Debug --output-on-failure
 # REM ./build\bin\Debug\SimulationTest.exe
