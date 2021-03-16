24704f661c3a5fcb76607e60ba988b2108536cec
FriggyStudios <friggystudios@gmail.com>
2021-03-16 22:28:50 +0000

    fail on not tests found

--- test.sh
+++ test.sh
@@ -5,5 +5,6 @@ then
    exit 1
 fi
 cd build
-ctest -C Debug --output-on-failure
+ctest -C Debug --output-on-failure .. 2>ctest.error
+if grep "No tests were found" ctest.error  ; then exit 1 ; fi
 # REM ./build\bin\Debug\SimulationTest.exe
