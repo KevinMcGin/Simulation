e15b20c0614e4bd9f49804a4284953257b18288d
FriggyStudios <friggystudios@gmail.com>
2021-03-20 11:26:29 +0000

    fix build

--- SimulationMain/main.cu
+++ SimulationMain/main.cu
@@ -8,11 +8,21 @@
 #include "ParticleDistributionDisk.h"
 #include "Timing.h"
 
+#include <ctype.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <tchar.h>
 
-int main()
+//Windows only
+// #include "XGetopt.h"
+//Unix only
+//#include <unistd.h>
+
+
+int main(int argc, TCHAR *argv[])
 {
 	Timing::setTime();
+	TCHAR* x = argv[1];
 
 	#ifdef USE_CUDA
     	std::cout << "CUDA: On" << std::endl;
@@ -20,14 +30,68 @@ int main()
     	std::cout << "CUDA: Off" << std::endl;
 	#endif
 
-	unsigned long particleCount = 100;
+	unsigned long particleCount = 50;
 	unsigned int frameRate = 60;
-	unsigned int endTime = 1 * 10 * frameRate;
+	double seconds = 10;
 	double meanMass = 0.01;	
 	double starMass = 50;
 	double meanSpeed = 0.04;
-	double deltaSpeed = meanSpeed * 0.2;
+	double deltaSpeedFraction = 0.2;
 	double outerRadius = 15;
+
+
+	//Specifying the expected options
+    //The two options l and b expect numbers as argument
+    // static struct option long_options[] = {
+    //     {"particleCount",      no_argument,       	0,  'c' },
+    //     {"frameRate", 	       no_argument,       	0,  'f' },
+    //     {"seconds",            required_argument, 	0,  's' },
+    //     {"meanMass",           required_argument, 	0,  'm' },
+    //     {"starMass",           required_argument, 	0,  't' },
+    //     {"deltaSpeedFraction", required_argument,   0,  'd' },
+    //     {"outerRadius",        required_argument, 	0,  'r' },
+    //     {0,             	   0,                 	0,  0   }
+    // };
+
+	// int c;
+	// while ((c = getopt(argc, argv, _T("c:f:s:m:t:v:d:r:"))) != EOF)
+	// {
+	// 	switch (c)
+	// 	{
+	// 		case _T('c'):
+	// 			particleCount = atoi(optarg);
+	// 			break;				
+	// 		case _T('f'):
+	// 			frameRate = atoi(optarg);
+	// 			break;
+	// 		case _T('s'): 
+	// 			seconds = atoi(optarg);
+	// 			break;
+	// 		case _T('m'):
+	// 			meanMass = atoi(optarg);
+	// 			break;				
+	// 		case _T('t'):
+	// 			starMass = atoi(optarg);
+	// 			break;				
+	// 		case _T('v'):
+	// 			meanSpeed = atoi(optarg);
+	// 			break;
+	// 		case _T('d'):
+	// 			deltaSpeedFraction = atoi(optarg);
+	// 			break;
+	// 		case _T('r'):
+	// 			outerRadius = atoi(optarg);
+	// 			break;				
+	// 		case _T('?'):
+	// 			break;
+	// 		default:
+	// 			break;
+	// 	}
+	// }
+
+	double deltaSpeed = meanSpeed * deltaSpeedFraction;
+	unsigned int endTime = seconds * frameRate;
+
 	Vector3D meanPosition = {0,0,0};
 	Distribution* massDistrubtion = new DistributionSimple(meanMass, meanMass*0.9);
 	Distribution* density = new DistributionValue(3000);
