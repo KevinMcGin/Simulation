4e9470061e76c3b446a251edc15b311394231f54
FriggyStudios <friggystudios@gmail.com>
2021-12-11 17:41:19 +0000

    project wide config file. config files created on compile

--- compile.sh
+++ compile.sh
@@ -10,6 +10,10 @@ do_delete_build() {
     mkdir build
 }
 
+create_config_maybe() {
+   test -f $1 || (cp $1.example $1 && echo "Created config file: $1")
+}
+
 while getopts 'd' flag; do
   case "${flag}" in
     d) delete_build='true' ;;
@@ -29,9 +33,17 @@ then
    echo -e "\ncompile failed"
    exit 1
 fi
+
 cmake --build build
 if [ $? -ne 0 ]
 then
    echo -e "\nbuild failed"
    exit 1
 fi
+
+create_config_maybe config/project.config
+create_config_maybe config/engine/engine.config
+
+# TODO: link these instead of copying the file (move to compile?)
+source ./config/project.config
+cp SimulationRenderer/lib/freeglut/bin/x64/freeglut.dll ./${BUILD_PATH:-'build/bin'}
