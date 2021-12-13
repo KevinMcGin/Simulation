b42d0def39df0c4ce09353092c3bf2f4dd6ac56c
FriggyStudios <friggystudios@gmail.com>
2021-12-13 20:59:00 +0000

    use filereadstream

--- SimulationRenderer/InputJSON.cpp
+++ SimulationRenderer/InputJSON.cpp
@@ -9,19 +9,28 @@
 #include <rapidjson/writer.h>
 #include <rapidjson/stringbuffer.h>
 #include <rapidjson/ostreamwrapper.h>
+#include "rapidjson/filereadstream.h"
 
 InputJSON::InputJSON(string fileName): 
 	time(0)
 {
-	ifstream ifs(fileName);
-	if ( !ifs.is_open() )
-    {
-        std::cerr << "Could not open file for reading!\n";
-    }
+	FILE* fp;
+	#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+		char* mode = "rb";
+	#else
+		char* mode = "r";
+	#endif
+	fopen_s(&fp, fileName.c_str(), mode);
+ 
+	char readBuffer[65536];
+	FileReadStream is(fp, readBuffer, sizeof(readBuffer));
+	// if ( !is.() )
+    // {
+    //     std::cerr << "Could not open file for reading!\n";
+    // }
 
-    IStreamWrapper isw { ifs };
 	std::cout << "parsing" << std::endl;
-    doc.ParseStream( isw );
+    doc.ParseStream( is );
 	std::cout << "parsed" << std::endl;
 	
 }
@@ -30,13 +39,13 @@ InputJSON::~InputJSON() { }
 
 Value* InputJSON::input() {
 	char timeString[11]; 
-	sprintf(timeString,"%ld", time++);
+	sprintf_s(timeString,"%ld", time++);
 	if(doc.HasMember(timeString)) {
 		return &doc[timeString];
 	} else  {
 		char timeString2[11]; 
 		time = time - 2;
-		sprintf(timeString2,"%ld", time);
+		sprintf_s(timeString2,"%ld", time);
 		return &doc[timeString2];
 	}
 }
