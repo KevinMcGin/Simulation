d73e4704e07fcb6b756557bd6dd298d01b785cd8
FriggyStudios <friggystudios@gmail.com>
2021-12-30 18:31:23 +0000

    Collision tests to sort the particles by mass, position, velocity, and then do expectations #135

--- SimulationTest/cpuTests/CollisionTest.cpp
+++ SimulationTest/cpuTests/CollisionTest.cpp
@@ -4,6 +4,48 @@
 #include "CollisionResolverCoalesce.cuh"
 #include "ParticleSimple.h"
 
+#include <algorithm>
+
+template <typename T>
+bool sortVector(T const &lhs, T const &rhs, vector<double*> compareLhs, vector<double*> compareRhs) {
+	if (*(compareLhs.back()) < *(compareRhs.back())) {
+      	return true; 
+	} else if (*(compareRhs.back()) < *(compareLhs.back())) {
+      	return false;
+	} else {
+		if(compareLhs.size() == 1) {
+			return true;
+		}
+		compareLhs.pop_back();
+		compareRhs.pop_back();
+		return sortVector(lhs, rhs, compareLhs, compareRhs);
+	}
+}
+
+bool sortParticles(Particle* const &lhs, Particle* const &rhs) {
+	auto compareLhs = vector<double*> {
+		&(lhs->velocity.z),
+		&(lhs->velocity.y),
+		&(lhs->velocity.x),
+		&(lhs->position.z),
+		&(lhs->position.y),
+		&(lhs->position.x),
+		&(lhs->radius),
+		&(lhs->mass)
+	};
+	auto compareRhs = vector<double*> {
+		&(rhs->velocity.z),
+		&(rhs->velocity.y),
+		&(rhs->velocity.x),
+		&(rhs->position.z),
+		&(rhs->position.y),
+		&(rhs->position.x),
+		&(rhs->radius),
+		&(rhs->mass)
+	};
+	return sortVector(*lhs, *rhs, compareLhs, compareRhs);
+}
+
 TEST(CollisionTest, ParticlesCollide) {
 	Collision law(new CollisionDetectorSimple(), new CollisionResolverCoalesce());
 	Particle* p1 = new ParticleSimple(1, 1, { 2,1,0 }, { 1,-1,0 }, { 0,0,0 });
@@ -11,12 +53,13 @@ TEST(CollisionTest, ParticlesCollide) {
 	Particle* p3 = new ParticleSimple(1, 1, { 20,0,0 }, { 1,1,0 }, { 0,0,0 });
 	vector<Particle*> particles = { p1, p2, p3 };
 	law.cpuRun(particles);
+	std::sort(particles.begin(), particles.end(), sortParticles);
 	EXPECT_EQ(2, particles.size());
-	EXPECT_EQ(2, particles[0]->mass);
-	EXPECT_DOUBLE_EQ(1.2599210498948732, particles[0]->radius);
-	EXPECT_EQ(Vector3D(2,0.5,0), particles[0]->position);
-	EXPECT_EQ(Vector3D(1,0,0), particles[0]->velocity);
-	EXPECT_EQ(Vector3D(0,0,0), particles[0]->angularVelocity);
+	EXPECT_EQ(2, particles[1]->mass);
+	EXPECT_DOUBLE_EQ(1.2599210498948732, particles[1]->radius);
+	EXPECT_EQ(Vector3D(2,0.5,0), particles[1]->position);
+	EXPECT_EQ(Vector3D(1,0,0), particles[1]->velocity);
+	EXPECT_EQ(Vector3D(0,0,0), particles[1]->angularVelocity);
 }
 
 TEST(CollisionTest, MultipleParticlesAllCollide) {
@@ -27,12 +70,13 @@ TEST(CollisionTest, MultipleParticlesAllCollide) {
 	Particle* p4 = new ParticleSimple(1, 1, { 2,0,0 }, { 1,1,0 }, { 0,0,0 });
 	vector<Particle*> particles = { p1, p2, p3, p4 };
 	law.cpuRun(particles);
+	std::sort(particles.begin(), particles.end(), sortParticles);
 	EXPECT_EQ(2, particles.size());
-	EXPECT_EQ(3, particles[0]->mass);
-	EXPECT_DOUBLE_EQ(1.4422495703074083, particles[0]->radius);
-	EXPECT_EQ(Vector3D(2,1/3.0,0), particles[0]->position);
-	EXPECT_EQ(Vector3D(1,1/3.0,0), particles[0]->velocity);
-	EXPECT_EQ(Vector3D(0,0,0), particles[0]->angularVelocity);
+	EXPECT_EQ(3, particles[1]->mass);
+	EXPECT_DOUBLE_EQ(1.4422495703074083, particles[1]->radius);
+	EXPECT_EQ(Vector3D(2,1/3.0,0), particles[1]->position);
+	EXPECT_EQ(Vector3D(1,1/3.0,0), particles[1]->velocity);
+	EXPECT_EQ(Vector3D(0,0,0), particles[1]->angularVelocity);
 }
 
 TEST(CollisionTest, MultipleParticlesPartialCollide) {
@@ -44,14 +88,15 @@ TEST(CollisionTest, MultipleParticlesPartialCollide) {
 	Particle* p5 = new ParticleSimple(1, 1, { 400,0,0 }, { 1,1,0 }, { 0,0,0 });
 	vector<Particle*> particles = { p1, p2, p3, p4, p5 };
 	law.cpuRun(particles);
+	std::sort(particles.begin(), particles.end(), sortParticles);
 	EXPECT_EQ(3, particles.size());
-	EXPECT_EQ(3, particles[0]->mass);
+	EXPECT_EQ(1, particles[0]->mass);
 	EXPECT_EQ(1, particles[1]->mass);
-	EXPECT_EQ(1, particles[2]->mass);
-	EXPECT_DOUBLE_EQ(1.4422495703074083, particles[0]->radius);
-	EXPECT_EQ(Vector3D(0.5,0.0,0), particles[0]->position);
-	EXPECT_EQ(Vector3D(1,1/3.0,0), particles[0]->velocity);
-	EXPECT_EQ(Vector3D(0,0,0), particles[0]->angularVelocity);
+	EXPECT_EQ(3, particles[2]->mass);
+	EXPECT_DOUBLE_EQ(1.4422495703074083, particles[2]->radius);
+	EXPECT_EQ(Vector3D(0.5,0.0,0), particles[2]->position);
+	EXPECT_EQ(Vector3D(1,1/3.0,0), particles[2]->velocity);
+	EXPECT_EQ(Vector3D(0,0,0), particles[2]->angularVelocity);
 }
 
 TEST(CollisionTest, MultipleParticlesIndependentlyCollide) {
@@ -63,12 +108,13 @@ TEST(CollisionTest, MultipleParticlesIndependentlyCollide) {
 	Particle* p5 = new ParticleSimple(1, 1, { 400,0,0 }, { 1,1,0 }, { 0,0,0 });
 	vector<Particle*> particles = { p1, p2, p3, p4, p5 };
 	law.cpuRun(particles);
+	std::sort(particles.begin(), particles.end(), sortParticles);
 	EXPECT_EQ(3, particles.size());
-	EXPECT_EQ(2, particles[0]->mass);
-	EXPECT_EQ(2.5, particles[1]->mass);
-	EXPECT_EQ(1, particles[2]->mass);
-	EXPECT_DOUBLE_EQ(1.2599210498948732, particles[0]->radius);
-	EXPECT_EQ(Vector3D(0.25,0.0,0), particles[0]->position);
-	EXPECT_EQ(Vector3D(1,0,0), particles[0]->velocity);
-	EXPECT_EQ(Vector3D(0,0,0), particles[0]->angularVelocity);
+	EXPECT_EQ(2, particles[1]->mass);
+	EXPECT_EQ(2.5, particles[2]->mass);
+	EXPECT_EQ(1, particles[0]->mass);
+	EXPECT_DOUBLE_EQ(1.2599210498948732, particles[1]->radius);
+	EXPECT_EQ(Vector3D(0.25,0.0,0), particles[1]->position);
+	EXPECT_EQ(Vector3D(1,0,0), particles[1]->velocity);
+	EXPECT_EQ(Vector3D(0,0,0), particles[1]->angularVelocity);
 }
