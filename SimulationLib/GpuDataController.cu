3b42dc4441bf94e99a2161243799baef163b548f
FriggyStudios <friggystudios@gmail.com>
2021-12-31 19:53:44 +0000

    Detect class type of Particle for memory allocating on GPU (instead of assuming is alway ParticleSimple) #81

--- SimulationLib/GpuDataController.cu
+++ SimulationLib/GpuDataController.cu
@@ -8,6 +8,11 @@ GpuDataController::GpuDataController() {
 
 GpuDataController::~GpuDataController() {
     delete cudaWithError;
+	for(int i = 0; i < particleCount; i++) {
+		cudaWithError->free(d_par[i]);
+	}
+	cudaWithError->free(td_par);
+	delete d_par;
 }
 
 void GpuDataController::putParticlesOnDevice(vector<Particle*> particles, bool firstRun) {
@@ -24,19 +29,19 @@ void GpuDataController::putParticlesOnDevice(vector<Particle*> particles, bool f
 	//Copy dynamically allocated child objects to GPU
 	d_par = new Particle*[particleCount];
 	for(int i = 0; i < particleCount; ++i) {
-		cudaWithError->malloc((void**)&d_par[i], sizeof(ParticleSimple));
-		cudaWithError->memcpy(d_par[i], particles[i], sizeof(ParticleSimple), cudaMemcpyHostToDevice);
+		cudaWithError->malloc((void**)&d_par[i], sizeof(*particles[i]));
+		cudaWithError->memcpy(d_par[i], particles[i], sizeof(*particles[i]), cudaMemcpyHostToDevice);
 	}
 
 	//Copy the d_par array itself to the device
-	cudaWithError->malloc((void**)&td_par, particleCount * sizeof(Particle *));
-	cudaWithError->memcpy(td_par, d_par, particleCount * sizeof(Particle *), cudaMemcpyHostToDevice);
+	cudaWithError->malloc((void**)&td_par, particleCount * sizeof(Particle*));
+	cudaWithError->memcpy(td_par, d_par, particleCount * sizeof(Particle*), cudaMemcpyHostToDevice);
 }
 
 void GpuDataController::getParticlesFromDevice(vector<Particle*>& particles) {
 	cudaWithError->deviceSynchronize();
 	for(int i = 0; i < particleCount; i++) {
-		cudaWithError->memcpy(particles[i], d_par[i], sizeof(ParticleSimple), cudaMemcpyDeviceToHost);
+		cudaWithError->memcpy(particles[i], d_par[i], sizeof(*particles[i]), cudaMemcpyDeviceToHost);
 	}
 }
 
