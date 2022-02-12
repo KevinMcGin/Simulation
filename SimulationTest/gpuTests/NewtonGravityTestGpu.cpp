deb864e37d2fb87c6770862067064a383c9813cf
FriggyStudios <friggystudios@gmail.com>
2022-02-12 08:48:55 +0000

    Gravity stress test (high number of particles) #170

--- SimulationTest/gpuTests/NewtonGravityTestGpu.cpp
+++ SimulationTest/gpuTests/NewtonGravityTestGpu.cpp
@@ -10,6 +10,17 @@ TEST(NewtonGravityTest, ParticlesAccelerateGpu) {
 	NewtonGravityTestHelper::testParticlesAccelerate(particles);
 }
 
+TEST(NewtonGravityTest, ManyParticlesAccelerateGpu) {
+	Law* law = new NewtonGravity(0.5);
+	int particleCount = 30 * 1000;
+	vector<Particle*> particles = LawHelper::setupParticles(particleCount);
+	auto p1 = particles[20 * 1000];
+	EXPECT_EQ(Vector3D(-20000, 40000, -40000), p1->velocity);
+    LawHelper::runGpuLaw(law, particles);
+	auto p2 = particles[2 * 1000];
+	EXPECT_EQ(Vector3D(-1999.8196813033317, 3999.8196813033546, -3999.8196813033546), p2->velocity);
+}
+
 TEST(NewtonGravityTest, ParticlesAccelerateGpuLikeCpu) {
 	const int particleCount = 300;
 	const int stepsCount = 2;
