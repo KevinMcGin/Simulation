827ee5b39bb313d434f95e3f0909bf4d00c64053
Kevin McGinley <kevinmcgin@gmail.com>
2024-02-20 21:29:37 +0000

    Update SimulationOutputJSON.cpp

--- SimulationEngineLib/universe/output/SimulationOutputJSON.cpp
+++ SimulationEngineLib/universe/output/SimulationOutputJSON.cpp
@@ -2,47 +2,36 @@
 
 #include "universe/output/SimulationOutputJSON.h"
 
-SimulationOutputJSON::SimulationOutputJSON(const char* outputFile) : SimulationOutput() {
-	myfile.open(outputFile);
+SimulationOutputJSON::SimulationOutputJSON(const char* outputFile) : SimulationOutput(outputFile) {}
+
+SimulationOutputJSON::~SimulationOutputJSON() {
+	appendToBuffer("}");
 }
 
-SimulationOutputJSON::~SimulationOutputJSON()
-{
-	myfile << buffer << "}" << std::endl;
-	myfile.close();
-}
-
-void SimulationOutputJSON::output(std::vector<Particle*> particles, unsigned long time)
-{
+void SimulationOutputJSON::output(std::vector<Particle*> particles, unsigned long time) {
 	if (time > 0) {
-		buffer = buffer.append(",\n");
+		appendToBuffer(",\n");
 	}
-	buffer = buffer.append("\"")
-			.append(std::to_string(time))
-			.append("\": [\n");
+	appendToBuffer("\"")
+	appendToBuffer(std::to_string(time))
+	appendToBuffer("\": [\n");
 	unsigned int particleCount = particles.size();
 	unsigned int i = 0;
 	for (const auto& p : particles) {
-		buffer = buffer.append("{ ")
-			.append("\"r\": ")
-			.append(std::to_string(p->radius))
-			.append(",\n")
-			.append("\"pos\": [")
-			.append(std::to_string(p->position.x))
-			.append(",")
-			.append(std::to_string(p->position.y))
-			.append(",")
-			.append(std::to_string(p->position.z))
-			.append("]}\n");
+		appendToBuffer("{ ")
+		appendToBuffer("\"r\": ")
+		appendToBuffer(std::to_string(p->radius))
+		appendToBuffer(",\n")
+		appendToBuffer("\"pos\": [")
+		appendToBuffer(std::to_string(p->position.x))
+		appendToBuffer(",")
+		appendToBuffer(std::to_string(p->position.y))
+		appendToBuffer(",")
+		appendToBuffer(std::to_string(p->position.z))
+		appendToBuffer("]}\n");
 		if (++i < particleCount) {
-			buffer = buffer.append(",\n");
+			appendToBuffer(",\n");
 		}
 	}
-	buffer = buffer.append("]");
-
-	auto bufferLength = buffer.length();
-	if (bufferLength >= maxBufferLength) {
-		myfile << buffer;
-		buffer = "";
-	}
+	appendToBuffer("]");
 }
