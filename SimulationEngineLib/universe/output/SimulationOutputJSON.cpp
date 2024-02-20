053de16ad0c5f2f8c7b046836270961ece9def8a
Kevin McGinley <kevinmcgin@gmail.com>
2024-02-20 21:32:20 +0000

    Update SimulationOutputJSON.cpp

--- SimulationEngineLib/universe/output/SimulationOutputJSON.cpp
+++ SimulationEngineLib/universe/output/SimulationOutputJSON.cpp
@@ -12,22 +12,22 @@ void SimulationOutputJSON::output(std::vector<Particle*> particles, unsigned lon
 	if (time > 0) {
 		appendToBuffer(",\n");
 	}
-	appendToBuffer("\"")
-	appendToBuffer(std::to_string(time))
+	appendToBuffer("\"");
+	appendToBuffer(std::to_string(time));
 	appendToBuffer("\": [\n");
 	unsigned int particleCount = particles.size();
 	unsigned int i = 0;
 	for (const auto& p : particles) {
-		appendToBuffer("{ ")
-		appendToBuffer("\"r\": ")
-		appendToBuffer(std::to_string(p->radius))
-		appendToBuffer(",\n")
-		appendToBuffer("\"pos\": [")
-		appendToBuffer(std::to_string(p->position.x))
-		appendToBuffer(",")
-		appendToBuffer(std::to_string(p->position.y))
-		appendToBuffer(",")
-		appendToBuffer(std::to_string(p->position.z))
+		appendToBuffer("{ ");
+		appendToBuffer("\"r\": ");
+		appendToBuffer(std::to_string(p->radius));
+		appendToBuffer(",\n");
+		appendToBuffer("\"pos\": [");
+		appendToBuffer(std::to_string(p->position.x));
+		appendToBuffer(",");
+		appendToBuffer(std::to_string(p->position.y));
+		appendToBuffer(",");
+		appendToBuffer(std::to_string(p->position.z));
 		appendToBuffer("]}\n");
 		if (++i < particleCount) {
 			appendToBuffer(",\n");
