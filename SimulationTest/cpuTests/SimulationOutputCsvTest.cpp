605f1a21309e1863550c37e478947f00fc8ef6b9
FriggyStudios <friggystudios@gmail.com>
2024-02-21 15:52:16 +0000

    SimulationOutputCsvTest

---
+++ SimulationTest/cpuTests/SimulationOutputCsvTest.cpp
@@ -0,0 +1,14 @@
+#include  "gtest/gtest.h"
+#include "universe/output/SimulationOutputCsv.h"
+#include "particle/ParticleSimple.h"
+
+TEST(SimulationOutputCsvTest, SimulationOutputtedToCsv) {
+	SimulationOutputCsv simulationOutputCsv("simulation_output/SimulationOutputCsvTest_SimulationOutputtedToCsv.json");
+    std::vector<Particle*> particles = {
+        new ParticleSimple(1,1,{0,0.1,0},{0,0,0}),
+        new ParticleSimple(2,2,{2,-0.2,0},{0,0,0})
+    };
+	simulationOutputCsv.output(particles, 0);
+	simulationOutputCsv.output(particles, 1);
+}
+
