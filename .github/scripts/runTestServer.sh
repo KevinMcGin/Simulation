8d07ec7480038cf496fcc2910f40f1b6bb39e9c0
Kevin McGinley <kevinmcgin@gmail.com>
2024-08-29 16:02:20 +0100

    fix bash curl code and returns 1 if curl fails

---
+++ .github/scripts/runTestServer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+testResultId=$(curl https://api.fhionnghaile.ie/api/sim/test/${{ github.sha }}/commit -H "TEST-TOKEN:test");
+echo $testResultId;
+if [[ $testResultId == *"error"* ]]; then
+  exit 1;
+fi
+testResult=$(curl https://api.fhionnghaile.ie/api/sim/test/$testResultId/result -H "TEST-TOKEN:test");
+if [[ $testResult == *"error"* ]]; then
+  exit 1;
+fi
+isReady=$(echo $testResult | jq -r '.isReady');
+while [ $isReady = "false" ]; do
+    sleep 10;
+    testResult=$(curl https://api.fhionnghaile.ie/api/sim/test/$testResultId/result -H "TEST-TOKEN:test");
+    if [[ $testResult == *"error"* ]]; then
+        exit 1;
+    fi
+    isReady=$(echo $testResult | jq -r '.isReady');
+done
+echo $(echo $testResult | jq -r '.message');
+isSuccess=$(echo $testResult | jq -r '.isSuccess');
+if [ $isSuccess = "false" ]; then
+    exit 1;
+fi
+exit 0;
