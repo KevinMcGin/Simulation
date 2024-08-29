8ce675fee8cafd8f9859c31af4e0843ccb21b1a5
Kevin McGinley <kevinmcgin@gmail.com>
2024-08-29 17:02:15 +0100

    fi not fi;

--- .github/scripts/runTestServer.sh
+++ .github/scripts/runTestServer.sh
@@ -7,19 +7,19 @@ fi;
 testResult=$(curl https://api.fhionnghaile.ie/api/sim/test/$testResultId/result -H "TEST-TOKEN:test");
 if [[ $testResult == *"error"* ]]; then
   exit 1;
-fi;
+fi
 isReady=$(echo $testResult | jq -r '.isReady');
 while [ $isReady = "false" ]; do
     sleep 10;
     testResult=$(curl https://api.fhionnghaile.ie/api/sim/test/$testResultId/result -H "TEST-TOKEN:test");
     if [[ $testResult == *"error"* ]]; then
         exit 1;
-    fi;
+    fi
     isReady=$(echo $testResult | jq -r '.isReady');
 done
 echo $(echo $testResult | jq -r '.message');
 isSuccess=$(echo $testResult | jq -r '.isSuccess');
 if [ $isSuccess = "false" ]; then
     exit 1;
-fi;
+fi
 exit 0;
