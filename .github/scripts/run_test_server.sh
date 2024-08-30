e3497a8301f9616ddd1bb43de86d780a8d334b16
Kevin McGinley <kevinmcgin@gmail.com>
2024-08-30 10:32:01 +0100

    Only check is ready

--- .github/scripts/run_test_server.sh
+++ .github/scripts/run_test_server.sh
@@ -6,24 +6,19 @@ echo "testResultId: $testResultId";
 if [[ $testResultId = *"error"* ]]; then
   exit 1;
 fi
+sleep 10;
 testResult=$(curl https://api.fhionnghaile.ie/api/sim/test/$testResultId/result -H "tester-token:$test_token");
 echo "testResult: $testResult";
-if [[ $testResult = *"error"* ]]; then
-  exit 1;
-fi
 isReady=$(echo $testResult | jq -r '.isReady');
 while [ $isReady = "false" ]; do
     sleep 10;
     testResult=$(curl https://api.fhionnghaile.ie/api/sim/test/$testResultId/result -H "tester-token:$test_token");
     echo "testResult: $testResult";
-    if [[ $testResult = *"error"* ]]; then
-        exit 1;
-    fi
     isReady=$(echo $testResult | jq -r '.isReady');
 done
 echo $(echo $testResult | jq -r '.message');
 isSuccess=$(echo $testResult | jq -r '.isSuccess');
-if [ $isSuccess = "false" ]; then
-    exit 1;
+if [ $isSuccess = "true" ]; then
+    exit 0;
 fi
-exit 0;
+exit 1;
