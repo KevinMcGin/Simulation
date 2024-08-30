11229ec50ef65ac14f88fd9fa16ad68926ea08d9
FriggyStudios <friggystudios@gmail.com>
2024-08-30 18:12:53 +0100

    printf

--- .github/scripts/run_test_server.sh
+++ .github/scripts/run_test_server.sh
@@ -2,13 +2,13 @@
 github_sha=$1
 test_token=$2
 testResultId=$(curl https://api.fhionnghaile.ie/api/sim/test/$github_sha/commit -H "tester-token:$test_token");
-echo "testResultId: $testResultId";
+printf "testResultId: $testResultId";
 if [[ $testResultId = *"error"* ]]; then
   exit 1;
 fi
 sleep 10;
 testResult=$(curl https://api.fhionnghaile.ie/api/sim/test/$testResultId/result -H "tester-token:$test_token");
-echo -e "test result status: $testResult\n";
+printf "test result status: $testResult\n";
 isReady=$(echo $testResult | jq -r '.isReady');
 while [ $isReady = "false" ]; do
     sleep 10;
@@ -16,8 +16,8 @@ while [ $isReady = "false" ]; do
     echo "testResult: $testResult";
     isReady=$(echo $testResult | jq -r '.isReady');
 done
-echo -e "final test result message: $testResult\n";
-echo -e $(echo $testResult | jq -r '.message');
+printf "final test result message: $testResult\n";
+printf $(echo $testResult | jq -r '.message');
 isSuccess=$(echo $testResult | jq -r '.isSuccess');
 if [ $isSuccess = "true" ]; then
     exit 0;
