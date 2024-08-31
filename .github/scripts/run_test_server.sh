5eb21688b733cd4cd65103be7980855bbbe9bd0b
FriggyStudios <friggystudios@gmail.com>
2024-08-31 23:10:59 +0100

    use <<<

--- .github/scripts/run_test_server.sh
+++ .github/scripts/run_test_server.sh
@@ -9,16 +9,17 @@ fi
 sleep 10;
 testResult=$(curl https://api.fhionnghaile.ie/api/sim/test/$testResultId/result -H "tester-token:$test_token");
 printf "test result status: $testResult\n";
-isReady=$($testResult | jq '.isReady');
+
+isReady=$( jq -r  '.isReady' <<< "${testResult}" )
 while [[ $isReady = false ]]; do
     sleep 10;
     testResult=$(curl https://api.fhionnghaile.ie/api/sim/test/$testResultId/result -H "tester-token:$test_token");
     printf "test result status: $testResult\n";
-isReady=$($testResult | jq '.isReady');
+    isReady=$( jq -r  '.isReady' <<< "${testResult}" )
 done
 printf "final test result: \n";
 printf $testResult;
-isSuccess=$($testResult | jq '.isSuccess');
+isSuccess=$( jq -r  '.isSuccess' <<< "${testResult}" )
 if [[ $isSuccess = true ]]; then
     exit 0;
 fi
