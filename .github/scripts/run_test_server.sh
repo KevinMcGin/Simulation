0637186f1365891cfe195d203196e77acacb9b7c
Kevin McGinley <kevinmcgin@gmail.com>
2024-09-03 17:31:53 +0100

    50 max retires and underscores in bash run test server sh

--- .github/scripts/run_test_server.sh
+++ .github/scripts/run_test_server.sh
@@ -3,28 +3,31 @@ github_sha=$1
 test_token=$2
 test_endpoint="https://api.fhionnghaile.ie/api/sim/test"
 tester_token_header="tester-token:$test_token"
-testResultId=$(curl $test_endpoint/$github_sha/commit -H $tester_token_header);
-printf "testResultId: $testResultId";
-if [[ $testResultId = *"error"* ]]; then
+test_result_id=$(curl $test_endpoint/$github_sha/commit -H $tester_token_header);
+printf "test_result_id: $test_result_id";
+if [[ "$test_result_id" = *"error"* ]]; then
   exit 1;
 fi
 
-testResult="";
-testStatus="RUNNING";
+test_result="";
+test_status="RUNNING";
+tries=0;
+max_tries=50;
 get_test_result() {
   sleep 10;
-  testResult=$(curl $test_endpoint/$testResultId/result -H $tester_token_header);
-  printf "test result status: $testResult\n";
-  testStatus=$( jq -r  '.testStatus' <<< "${testResult}" );
-  printf "testStatus: $testStatus\n";
+  test_result=$(curl $test_endpoint/$test_result_id/result -H $tester_token_header);
+  printf "test result status: $test_result\n";
+  test_status=$( jq -r  '.testStatus' <<< "${test_result}" );
+  printf "test_status: $test_status\n";
+  tries=$((tries + 1));
 }
 
-while [[ $testStatus = *"RUNNING"* ]]; do
+while [[ "$test_status" = *"RUNNING"* && tries < max_tries ]]; do
   get_test_result;
 done
 printf "final test result: \n";
-printf $testResult;
-if [[ $testStatus = *"SUCCESS"* ]]; then
+printf $test_result;
+if [[ "$test_status" = *"SUCCESS"* ]]; then
     exit 0;
 fi
 exit 1;
