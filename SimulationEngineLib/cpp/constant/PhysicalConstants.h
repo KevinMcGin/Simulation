e98c7b6b796409991f07fa11d569ed1616e927ca
Kevin McGinley <kevinmcgin@gmail.com>
2024-10-19 12:00:30 +0100

    gravity back to 9.81 m/s^2

--- SimulationEngineLib/cpp/constant/PhysicalConstants.h
+++ SimulationEngineLib/cpp/constant/PhysicalConstants.h
@@ -2,6 +2,6 @@
 
 class PhysicalConstants {
 public:
-    static constexpr double GRAVITATIONAL_CONSTANT = 0.05;
+    static constexpr double GRAVITATIONAL_CONSTANT = 6.6743e-11;
     static constexpr double SPEED_OF_LIGHT = 299792458.0;
 };
