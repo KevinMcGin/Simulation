57701235d16ceeb8825d2298e1e7b0238cb6170a
Kevin McGinley <kevinmcgin@gmail.com>
2024-10-19 11:19:47 +0100

    GRAVITATIONAL_CONSTANT as double

--- SimulationEngineLib/cpp/constant/PhysicalConstants.h
+++ SimulationEngineLib/cpp/constant/PhysicalConstants.h
@@ -2,6 +2,6 @@
 
 class PhysicalConstants {
 public:
-    static constexpr float GRAVITATIONAL_CONSTANT = 6.6743e-11;
+    static constexpr double GRAVITATIONAL_CONSTANT = 6.6743e-11;
     static constexpr double SPEED_OF_LIGHT = 299792458.0;
 };
